f7046c61a3fbbddf144cd0e1772c67354057066a
Bibok, Andor <bibok.andor@gmail.com>
2016-03-30 00:25:42 +0200

    Added IAstNodeSerializer.h interface

---
+++ ast/inc/core/IAstNodeSerializer.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "AstBase.h"
+
+namespace ast
+{
+
+class Background;
+class Comment;
+class DataTable;
+class DocString;
+class Example;
+class Feature;
+class Location;
+class Scenario;
+class ScenarioOutline;
+class Step;
+class TableCell;
+class TableRow;
+class Tag;
+
+namespace core
+{
+
+class IAstNodeSerializer
+{
+public:
+    
+    virtual const string serialize(const Background& obj) = 0;
+    virtual const string serialize(const Comment& obj) = 0;
+    virtual const string serialize(const DataTable& obj) = 0;
+    virtual const string serialize(const DocString& obj) = 0;
+    virtual const string serialize(const Example& obj) = 0;
+    virtual const string serialize(const Feature& obj) = 0;
+    virtual const string serialize(const Location& obj) = 0;
+    virtual const string serialize(const Scenario& obj) = 0;
+    virtual const string serialize(const ScenarioOutline& obj) = 0;
+    virtual const string serialize(const Step& obj) = 0;
+    virtual const string serialize(const TableCell& obj) = 0;
+    virtual const string serialize(const TableRow& obj) = 0;
+    virtual const string serialize(const Tag& obj) = 0;
+
+    virtual const string serialize(const AstBase& obj) = 0;
+
+    virtual ~ISerializer() {};  //TODO: not sure if necessary, maybe the default (non-virtual) dtor is enough
+};
+
+}
+}
