f6ff23a9ed2ecefde489aa347004fb381daac302
Bibok, Andor <bibok.andor@gmail.com>
2016-03-29 23:56:25 +0200

    Changed int to unsigned in Location

--- ast/inc/Location.h
+++ ast/inc/Location.h
@@ -8,13 +8,13 @@ namespace ast
 class Location final : public core::AstBase
 {
 public:
-    inline int line() const { return _line; }
-    inline int column() const { return _column; }
-    Location(int line, int column) : _line(line), _column(column) {}
+    inline unsigned line() const { return _line; }
+    inline unsigned column() const { return _column; }
+    Location(unsigned line, unsigned column) : _line(line), _column(column) {}
 
 private:
-    const int _line;
-    const int _column;
+    const unsigned _line;
+    const unsigned _column;
 };
 
 }
