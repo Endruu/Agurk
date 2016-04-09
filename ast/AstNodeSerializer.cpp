7332c7339c8f8a2bfc43d79c641f5bf807368c58
Bibok, Andor <bibok.andor@gmail.com>
2016-04-09 17:07:36 +0200

    Finished implementing visit functions

--- ast/AstNodeSerializer.cpp
+++ ast/AstNodeSerializer.cpp
@@ -8,7 +8,32 @@ using json = nlohmann::json;
 
 struct ast::AstNodeSerializer::Impl
 {
-    json result;
+    static json serializeAst(ast::AstNode& obj)
+    {
+        AstNodeSerializer serializer;
+        obj.accept(serializer);
+        return serializer.pImpl->result;
+    }
+
+    static void setResult(ast::AstNodeSerializer& serializer, const json& result)
+    {
+        serializer.pImpl->result = result;
+    }
+
+    template<typename T>
+    static std::vector<json> serializeContainer(T& container)
+    {
+        std::vector<json> serialized;
+
+        serialized.reserve(container.size());
+
+        for (auto& elem : container)
+        {
+            serialized.push_back(serializeAst(elem));
+        }
+
+        return serialized;
+    }
 
     static json Impl::serializeAstNodeBase(ast::AstNode& obj, const std::string& type)
     {
@@ -30,24 +55,18 @@ struct ast::AstNodeSerializer::Impl
 
     static void serializeTagContainer(ast::detail::TagContainerBase& obj, json& j)
     {
-        std::vector<json> serialized;
-
-        serialized.reserve(obj.tags.size());
-
-        for (auto& tag : obj.tags)
-        {
-            serialized.push_back(serializeAst(tag));
-        }
-
-        j["tags"] = serialized;
+        j["tags"] = serializeContainer(obj.tags);
     }
 
-    static json serializeAst(ast::AstNode& obj)
+    static void serializeScenarioDefinition(ast::ScenarioDefinition& obj, json& j)
     {
-        AstNodeSerializer serializer;
-        obj.accept(serializer);
-        return serializer.pImpl->result;
+        serializeKwd(obj, j);
+        j["steps"] = serializeContainer(obj.steps);
     }
+
+
+private:
+    json result;
 };
 
 ast::AstNodeSerializer::AstNodeSerializer() : pImpl(new ast::AstNodeSerializer::Impl) {}
@@ -57,25 +76,33 @@ ast::AstNodeSerializer::~AstNodeSerializer() {}
 void ast::AstNodeSerializer::visit(ast::Background& obj)
 {
     auto j = Impl::serializeAstNodeBase(obj, "Background");
-    Impl::serializeKwd(obj, j);
+    Impl::serializeScenarioDefinition(obj, j);
+    Impl::setResult(*this, j);
 }
 
 
 void ast::AstNodeSerializer::visit(ast::Comment& obj)
 {
     auto j = Impl::serializeAstNodeBase(obj, "Comment");
+    j["text"] = obj.text;
+    Impl::setResult(*this, j);
 }
 
 
 void ast::AstNodeSerializer::visit(ast::DataTable& obj)
 {
     auto j = Impl::serializeAstNodeBase(obj, "DataTable");
+    j["rows"] = Impl::serializeContainer(obj.rows);
+    Impl::setResult(*this, j);
 }
 
 
 void ast::AstNodeSerializer::visit(ast::DocString& obj)
 {
     auto j = Impl::serializeAstNodeBase(obj, "DocString");
+    j["content"] = obj.content;
+    j["contentType"] = obj.contentType;
+    Impl::setResult(*this, j);
 }
 
 
@@ -83,6 +110,10 @@ void ast::AstNodeSerializer::visit(ast::Example& obj)
 {
     auto j = Impl::serializeAstNodeBase(obj, "Example");
     Impl::serializeKwd(obj, j);
+    Impl::serializeTagContainer(obj, j);
+    j["header"] = Impl::serializeAst(obj.header);
+    j["rows"] = Impl::serializeContainer(obj.rows);
+    Impl::setResult(*this, j);
 }
 
 
@@ -103,51 +134,64 @@ void ast::AstNodeSerializer::visit(ast::Feature& obj)
     }
 
     j["scenarioDefinitions"] = serialized;   //TODO: name is probably not scenarioDefinitions
-    
-    serialized.clear();
-    serialized.shrink_to_fit();
-    serialized.reserve(obj.scenarioDefinitions.size());
-    for (auto& comment : obj.comments)
-    {
-        //TODO: check ptr, but in theory it should never be empty
-        serialized.push_back(Impl::serializeAst(comment));
-    }
-    
-    j["comments"] = serialized;
 
-    pImpl->result = j;
+    j["comments"] = Impl::serializeContainer(obj.comments);
+
+    Impl::setResult(*this, j);
 }
 
 
 void ast::AstNodeSerializer::visit(ast::Scenario& obj)
 {
     auto j = Impl::serializeAstNodeBase(obj, "Scenario");
-    Impl::serializeKwd(obj, j);
+    Impl::serializeTagContainer(obj, j);
+    Impl::serializeScenarioDefinition(obj, j);
+    Impl::setResult(*this, j);
 }
 
 
 void ast::AstNodeSerializer::visit(ast::ScenarioOutline& obj)
 {
     auto j = Impl::serializeAstNodeBase(obj, "ScenarioOutline");
-    Impl::serializeKwd(obj, j);
+    Impl::serializeTagContainer(obj, j);
+    Impl::serializeScenarioDefinition(obj, j);
+    j["examples"] = Impl::serializeContainer(obj.examples);
+    Impl::setResult(*this, j);
 }
 
 
 void ast::AstNodeSerializer::visit(ast::Step& obj)
 {
     auto j = Impl::serializeAstNodeBase(obj, "Step");
+    j["keyword"] = obj.keyword;
+    j["text"] = obj.text;
+
+    if (obj.argument)
+    {
+        j["argument"] = Impl::serializeAst(*obj.argument);
+    }
+    else
+    {
+        j["argument"] = nullptr;
+    }
+
+    Impl::setResult(*this, j);
 }
 
 
 void ast::AstNodeSerializer::visit(ast::TableCell& obj)
 {
     auto j = Impl::serializeAstNodeBase(obj, "TableCell");
+    j["value"] = obj.value;
+    Impl::setResult(*this, j);
 }
 
 
 void ast::AstNodeSerializer::visit(ast::TableRow& obj)
 {
     auto j = Impl::serializeAstNodeBase(obj, "TableRow");
+    j["cells"] = Impl::serializeContainer(obj.cells);
+    Impl::setResult(*this, j);
 }
 
 
@@ -155,6 +199,5 @@ void ast::AstNodeSerializer::visit(ast::Tag& obj)
 {
     auto j = Impl::serializeAstNodeBase(obj, "Tag");
     j["name"] = obj.name;
-
-    pImpl->result = j;
+    Impl::setResult(*this, j);
 }
